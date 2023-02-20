bb59db354824de291465746824ed9fb321013948
Sadnessa <kequeue.8@gmail.com>
2023-02-20 16:53:34 +0200

    Added script, style and template to FilmCard.

---
+++ src/components/base/FilmCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <img :src="img">
+    <div class="card__description">
+      fffff
+    </div>
+    <div class="card__actions">
+      lddkdk
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    img: {
+      type: String,
+      default: "https://picsum.photos/400/200",
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+  .card {
+    max-width: 30%;
+    background-color: var(--white);
+    overflow: hidden;
+    border-radius: 6px;
+  }
+</style>
