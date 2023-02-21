618c3a470c1c090f808d8715929dd1372ef5ba6e
Sadnessa <kequeue.8@gmail.com>
2023-02-21 06:35:24 +0200

    Pinia fix.

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import './assets/variables.scss'
 import router from './router'
 import App from './App.vue'
 
-createApp(App).use(router).use(createPinia).mount('#app')
+const pinia = createPinia()
+
+createApp(App).use(router).use(pinia).mount('#app')
