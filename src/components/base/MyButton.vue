dc796393f5fcd1dcb5a6f0a24185fdec096aa044
Sadnessa <kequeue.8@gmail.com>
2023-02-21 04:41:14 +0200

    Created template, style and script for MyButton component.

---
+++ src/components/base/MyButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="button" :class="flat ? 'button--flat' : ''">
+    <slot/>
+  </button>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    flat: Boolean,
+  })
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    padding: 10px 8px;
+    background-color: var(--primary);
+    color: var(--white);
+    border-radius: 4px;
+    border: solid 2px var(--primary);  
+    cursor: pointer;
+
+    &--flat {
+      background-color: transparent;
+      color: var(--primary); 
+    }
+  }
+</style>
