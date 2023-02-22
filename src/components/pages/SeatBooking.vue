e4179ce620ef9e956dd6b880cc76c48442ebd655
Sadnessa <kequeue.8@gmail.com>
2023-02-22 09:25:12 +0200

    Changed template of SeatBooking page. Added logic and style.

--- src/components/pages/SeatBooking.vue
+++ src/components/pages/SeatBooking.vue
@@ -1,15 +1,67 @@
 <template>
   <section class="seat-booking">
     <div class="container">
-      <SeatSelect :seatsCount="40" :bookedSeats="[30, 4, 6]" v-model:selectedSeats="selectedSeats"/>
-      {{ selectedSeats }}
+      <div class="header">
+        <RouterLink to="/">
+          <MyButton>Назад</MyButton>
+        </RouterLink>
+        <h1>Film title</h1>
+      </div>
+      <SeatSelect
+        :seatsCount="40"
+        :bookedSeats="bookedSeats"
+        v-model:selectedSeats="selectedSeats"
+      />
+      <div class="column">
+        <p>
+          К оплате:
+          {{ `250 x ${selectedSeats.length} = ${250 * selectedSeats.length}` }}
+        </p>
+        <MyButton class="button" @click="onBookClick">Забронировать</MyButton>
+      </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-  import SeatSelect from '../base/SeatSelect.vue';
-  import { ref } from 'vue';
+import SeatSelect from "../base/SeatSelect.vue";
+import MyButton from "../base/MyButton.vue";
+import { ref } from "vue";
+import { useCards } from "../../store/store";
 
-  const selectedSeats = ref<number[]>([])
+const selectedSeats = ref<number[]>([]);
+const bookedSeats = ref<number[]>([30, 4, 6]);
+
+const onBookClick = () => {
+  bookedSeats.value = bookedSeats.value.concat(selectedSeats.value);
+  selectedSeats.value = [];
+};
 </script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+  gap: 20px;
+
+  h1 {
+    margin: 0;
+  }
+}
+
+.column {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  p {
+    margin: 0;
+    margin-top: 20px;
+  }
+
+  .button {
+    margin-top: 20px;
+  }
+}
+</style>
