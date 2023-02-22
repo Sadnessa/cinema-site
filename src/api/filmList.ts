741814de725f40e1500432b65897a330fce44d1c
Sadnessa <kequeue.8@gmail.com>
2023-02-22 10:36:35 +0200

    Added description to Film type.

--- src/api/filmList.ts
+++ src/api/filmList.ts
@@ -1,15 +1,85 @@
-export type Film = { id: number, title: string; price: number; img: string, seats: { count: number, booked: number[], available: number }};
+export type Film = {
+  id: number;
+  title: string;
+  price: number;
+  img: string;
+  description: string;
+  seats: { count: number; booked: number[]; available: number };
+};
 
 const films = [
-  { id: 1, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=1", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 2, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=2", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 3, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=3", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 4, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=4", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 5, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=5", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 6, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=6", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 7, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=7", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 8, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=8", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
-  { id: 9, title: "Lorem", price: 66, img: "https://picsum.photos/900/400?random=9", seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 } },
+  {
+    id: 1,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=1",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 2,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=2",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 3,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=3",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 4,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=4",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 5,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=5",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 6,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=6",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 7,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=7",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 8,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=8",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
+  {
+    id: 9,
+    title: "Lorem",
+    price: 66,
+    img: "https://picsum.photos/900/400?random=9",
+    description: "fkjdkfjskfjskfjskfsjfksfksjskjfks",
+    seats: { count: 40, booked: [10, 30, 22, 1, 4], available: 35 },
+  },
 ];
 
 // const films2 = new Array<Film>(6)
