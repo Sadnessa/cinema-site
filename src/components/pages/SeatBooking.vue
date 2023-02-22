ee6ffbcab213cf0c00dba17fd08b3eca11491113
Sadnessa <kequeue.8@gmail.com>
2023-02-22 05:30:53 +0200

    Added SeatSelect component to SeatBooking page.

--- src/components/pages/SeatBooking.vue
+++ src/components/pages/SeatBooking.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="seat-booking">
     <div class="container">
-      ldldl
+      <SeatSelect :seatsCount="40" :bookedSeats="[30, 4, 6]"/>
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+  import SeatSelect from '../base/SeatSelect.vue';
+</script>
