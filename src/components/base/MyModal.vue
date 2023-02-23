42bf3ba936577aa87e8df4d71e023b3519e887aa
Sadnessa <kequeue.8@gmail.com>
2023-02-23 05:31:34 +0200

    Created MyModal component.

---
+++ src/components/base/MyModal.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="modal" v-if="openModal">
+    <div class="modal__card">
+      <h1>
+        <slot name="header"></slot>
+      </h1>
+      <hr noshade />
+      <p><slot /></p>
+      <div class="modal__buttons">
+        <MyButton flat @click="onDecline">Отклонить</MyButton>
+        <MyButton @click="$emit('submitClick')">Подтвердить</MyButton>
+      </div>
+    </div>
+    <div class="modal__overlay"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import MyButton from "./MyButton.vue";
+import { computed } from "vue";
+
+const props = defineProps({
+  openModal: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const emits = defineEmits(["update:openModal"]);
+
+const openModal = computed({
+  get() {
+    return props.openModal;
+  },
+  set(value) {
+    emits("update:openModal", value);
+  },
+});
+
+const onDecline = () => {
+  openModal.value = false;
+};
+</script>
+
+<style lang="scss" scoped>
+.modal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+
+  &__card {
+    max-width: 300px;
+    background-color: var(--white);
+    border-radius: 6px;
+    padding: 20px;
+    text-align: center;
+
+    hr {
+      color: var(--light);
+    }
+  }
+
+  &__overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: var(--dark);
+    opacity: 0.4;
+    z-index: -10;
+  }
+}
+</style>
