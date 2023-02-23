d151728c29968d7e4029bb78c57dbae4601772d4
Sadnessa <kequeue.8@gmail.com>
2023-02-23 07:20:41 +0200

    Changed description template and style.

--- src/components/pages/Description.vue
+++ src/components/pages/Description.vue
@@ -1,13 +1,22 @@
 <template>
   <section class="description">
     <div class="container">
-      <img :src="card.img" />
-      <div class="content">
+      <div class="header">
+        <MyButton @click="$router.back()">Назад</MyButton>
         <h1>{{ card.title }}</h1>
-        <p> {{ card.description }}</p>
-        <RouterLink :to="{ path: '/seat-booking/', query: { film: card.id }}">
-          <MyButton>Купить билет</MyButton>
-        </RouterLink>
+      </div>
+      <div class="content">
+        <img :src="card.img" />
+        <div class="about">
+          <h1>{{ card.title }}</h1>
+          <p>{{ card.description }}</p>
+          <RouterLink
+            :to="{ path: '/seat-booking/', query: { film: card.id } }"
+          >
+            <MyButton>Купить билет</MyButton>
+          </RouterLink>
+          <span> {{ card.price }} </span>
+        </div>
       </div>
     </div>
   </section>
@@ -25,8 +34,19 @@ const card = computed(() => cardStore.cards[Number(route.query.film) - 1]);
 </script>
 
 <style lang="scss" scoped>
-  .container {
-    display: flex;
-    gap: 20px;
+.header {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 20px;
+
+  h1 {
+    margin: 0;
   }
+}
+
+.content {
+  display: flex;
+  gap: 20px;
+}
 </style>
