ee8f09d582f14eec4e2ef10ec2e6331a60c379e5
Sadnessa <kequeue.8@gmail.com>
2023-02-26 08:58:08 +0200

    Added transition to modal.

--- src/components/base/MyModal.vue
+++ src/components/base/MyModal.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="modal" v-if="openModal">
-    <div class="modal__card">
-      <h1 class="resizable">
-        <slot name="header"></slot>
-      </h1>
-      <hr noshade />
-      <div class="content">
-        <slot />
+  <Transition name="show" mode="out-in">
+    <div class="modal" v-if="openModal">
+      <div class="modal__card">
+        <h1 class="resizable">
+          <slot name="header"></slot>
+        </h1>
+        <hr noshade />
+        <div class="content">
+          <slot />
+        </div>
+        <div class="modal__buttons">
+          <MyButton flat @click="onDecline">Отклонить</MyButton>
+          <MyButton @click="$emit('submitClick')">Подтвердить</MyButton>
+        </div>
       </div>
-      <div class="modal__buttons">
-        <MyButton flat @click="onDecline">Отклонить</MyButton>
-        <MyButton @click="$emit('submitClick')">Подтвердить</MyButton>
-      </div>
+      <div class="modal__overlay"></div>
     </div>
-    <div class="modal__overlay"></div>
-  </div>
+  </Transition>
 </template>
 
 <script setup lang="ts">
@@ -84,6 +86,22 @@ const onDecline = () => {
     background-color: var(--dark);
     opacity: 0.4;
     z-index: -10;
+  }
+}
+
+//transition
+.show-enter-active,
+.show-leave-active {
+  transition: opacity 0.5s ease;
+  .modal__card {
+    transition: transform 0.5s ease;
+  }
+}
+.show-enter-from,
+.show-leave-to {
+  opacity: 0;
+  .modal__card {
+    transform: translateY(30px);
   }
 }
 </style>
