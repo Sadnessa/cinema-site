751bea531ebf4886ae0719772920783b9a48b7c4
Sadnessa <kequeue.8@gmail.com>
2023-02-26 05:03:52 +0200

    Added hover style for MyButton.

--- src/components/base/MyButton.vue
+++ src/components/base/MyButton.vue
@@ -13,6 +13,7 @@
 
 <style lang="scss" scoped>
   .button {
+    position: relative;
     padding: 10px 8px;
     background-color: var(--primary);
     color: var(--white);
@@ -21,11 +22,34 @@
     cursor: pointer;
     font-family: 'Montserrat', sans-serif;
     font-weight: 500;
+    overflow: hidden;
+
+    &::after {
+      position: absolute;
+      top: 0;
+      left: 0;
+      content: '';
+      width: 100%;
+      height: 100%;
+      background-color: var(--white);
+      opacity: 0;
+      transition: all ease-in-out 0.3s;
+    }
 
     &--flat {
       background-color: transparent;
       color: var(--primary); 
       border: solid 1px transparent;  
+
+      &::after {
+        background-color: var(--primary);
+      }
+    }
+
+    &:hover {
+      &::after {
+        opacity: 0.2;
+      }
     }
 
     &:disabled {
